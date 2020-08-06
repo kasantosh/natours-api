1859b660b0608b0250daf2e87cbffa484388984b
Santosh Kumar <kasantosh@yahoo.com>
2020-08-06 12:02:35 -0400

    Finished with the GET and POST methods

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+/////////// BASICS ////////////
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello from the server side',
+//     app: 'Natours',
+//   });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this endpoint...');
+// });
+/////////// BASICS END ////////////
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the server side',
-    app: 'Natours',
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
 });
 
 const port = 3000;
