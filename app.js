1fed495a00b12437644ae3e8fa3c4b16b87bbc43
Santosh Kumar <kasantosh@yahoo.com>
2020-08-10 08:45:44 -0400

    Added some middleware

--- app.js
+++ app.js
@@ -5,14 +5,26 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 // GET ALL TOURS FUNCTION
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    reqestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
@@ -105,20 +117,19 @@ const deleteTour = (req, res) => {
   });
 };
 
-// GET ALL TOURS
-app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
-// GET TOUR BY ID
-app.get('/api/v1/tours/:id', getTour);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
-// CREATE NEW TOUR (POST)
-app.post('/api/v1/tours', createTour);
-
-// UPDATE POST (PATCH)
-app.patch('/api/v1/tours/:id', updateTour);
-
-// DELETE TOUR
-app.delete('/api/v1/tours/:id', deleteTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
