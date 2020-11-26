d78c6e3875eb365ad914ff0994dff2c9823fdd11
Santosh <kasantosh@yahoo.com>
2020-11-26 09:58:01 -0500

    Completed the map part of the tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -6,13 +6,13 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  // 2) Build the template
+  // 2) Build the template in pug
 
   // 3) Render the template using tour data from 1)
 
-
     res.status(200).render('overview', {
-      tours: tours
+      title: 'All Tours',
+      tours
     });
   });
 
@@ -20,11 +20,13 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     //  Get the data for the requested tour (including reviews & Guides)
     const tour = await Tour.findOne({ slug: req.params.slug }).populate({
       path: 'reviews',
-      fields: 'review raing user'
+      fields: 'review rating user'
     });
-    console.log(tour);
+    // console.log(tour);
 
-    res.status(200).render('tour', {
+    // Render template using data
+    res.status(200).set('Content-Security-Policy', "default-src 'self' https://*.mapbox.com; base-uri 'self'; block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none'; script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    ).render('tour', {
       title: tour.name,
       tour
     });
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,16 +1,42 @@
-console.log('Hello from client')
-
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
 console.log(locations);
 
-//     mapboxgl.accessToken = 'pk.eyJ1Ijoia2FzYW50b3NoIiwiYSI6ImNrZ3V3cmU1MjBhOTAyeHFzcmQ4c3FyajAifQ._iO0W4tvpJqjx9_qd7RiBw';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FudG9zaDM4IiwiYSI6ImNraHhtbm81bjAzajYycm4wa3JmaWVtZHEifQ.q30dLnCdVomE4f_CJy8uEA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/santosh38/ckhxwhwxb190q19ps1stxmtne',
+    scrollZoom: false
+    // center: [-118.113491, 34.111745], //mapbox takes longitude first
+    // zoom: 10,
+    // interactive: false
+});
 
-//     var map = new mapboxgl.Map({
-//         container: 'map',
-//         style: 'mapbox://styles/mapbox/streets-v11',
-//         center: [-118.113491, 34.111745],
-//         zoom: 10
-//     });
-//     console.log(map)
+const bounds = new mapboxgl.LngLatBounds();
 
+locations.forEach(loc => {
+    // create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // add marker
+    new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom'
+    }).setLngLat(loc.coordinates).addTo(map);
+
+    // Add popup
+    new mapboxgl.Popup({ offset: 30 }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`).addTo(map);
+
+    // extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+        padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100
+    }
+});
