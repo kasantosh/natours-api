c9fc55d02b3d4f8717562a68ca3fc3882448aee7
Santosh <kasantosh@yahoo.com>
2021-01-19 12:56:56 -0500

    minor error in stripe fixed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -20,11 +20,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
-        line_items: [
+        display_items: [
             {
                 name: `${tour.name} Tour`,
                 description: tour.summary,
-                images: [ `https://www.natours.dev/img/tours/${tour.imageCover}` ],
+                images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
                 amount: tour.price * 100,
                 currency: 'usd',
                 quantity: 1
@@ -55,7 +55,7 @@ const createBookingCheckout = async session => {
     // below session object comes from session declared above top ⤴ 
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.display_items[0].amount / 100;
     await Booking.create({ tour, user, price })
 }
 
@@ -72,12 +72,10 @@ exports.webhookCheckout = (req, res, next) => {
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
-    if (event.type === 'checkout.session.completed') {
-        createBookingCheckout(event.data.object);
-        res.status(200).json({ received: true });
-    }
-   
-}
+    if (event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
+    res.status(200).json({ received: true });
+
+}   
 
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
